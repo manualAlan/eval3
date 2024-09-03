ba1b860860ad5793c91351b447c4b887f490945b
bm350 <bm350@duke.edu>
2024-09-03 18:48:37 -0400

    new

--- 014_complex_ty/mytypes.c
+++ 014_complex_ty/mytypes.c
@@ -4,6 +4,6 @@ struct complex_num_tag {
 };
 
 typedef struct complex_num_tag complex_num_t;
-enum animal_tag { CAT, FISH, DOG, HORSE };
+enum animal_tag { CAT, FISH, SNAKE, DOG, HORSE };
 
 typedef enum animal_tag animal_t;
