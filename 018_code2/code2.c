45708361a56f1dd40c0df1e22fbe71374d0ab22c
bm350 <bm350@duke.edu>
2024-09-07 15:43:55 -0400

    fef

--- 018_code2/code2.c
+++ 018_code2/code2.c
@@ -2,7 +2,7 @@
 int printTriangle(int size) {
   int starCount = 0;
   for (int i = 0; i < size; i++) {
-    for (int j = 0; j < i; j++) {
+    for (int j = 0; j <= i; j++) {
       printf("*");
       starCount++;
     }
