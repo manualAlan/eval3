ec7946efb66d680c174dfb605d23c5973d4723a6
bm350 <bm350@duke.edu>
2024-09-16 14:54:32 -0400

    afefeg

--- 031_tests_power/test-power.c
+++ 031_tests_power/test-power.c
@@ -18,11 +18,12 @@ void run_check(unsigned x, unsigned y, unsigned expected_ans) {
 }
 int main(void) {
   // Test cases
-  run_check(2, 3, 8);  // 2^3 = 8
-  run_check(0, 0, 1);
-  run_check(1, 100, 1);
-  run_check(5, 0, 1);
-  run_check(0, 5, 0);
+  run_check(2, 3, power(2, 3));
+  run_check(0, 0, power(0, 0));
+  run_check(1, 100, power(1, 100));
+  run_check(5, 0, power(5, 0));
+  run_check(0, 5, power(0, 5));
+  run_check(3, 4, power(3, 4));
 
   printf("All tests passed!\n");
   exit(EXIT_SUCCESS);
