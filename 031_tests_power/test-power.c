96a848a1b575f48dd0ea70fe163d982ceeb892de
bm350 <bm350@duke.edu>
2024-09-16 15:07:06 -0400

    afege

--- 031_tests_power/test-power.c
+++ 031_tests_power/test-power.c
@@ -3,12 +3,6 @@
 unsigned power(unsigned x, unsigned y);
 
 void run_check(unsigned x, unsigned y, unsigned expected_ans) {
-  if (x == 0 && y == 0) {
-    if (expected_ans != 1) {
-      printf("Test failed: power(%u, %u) = %u, expected %u\n", x, y, 0, expected_ans);
-      exit(EXIT_FAILURE);
-    }
-  }
   unsigned result = power(x, y);
 
   if (result != expected_ans) {
@@ -18,12 +12,19 @@ void run_check(unsigned x, unsigned y, unsigned expected_ans) {
 }
 int main(void) {
   // Test cases
-  run_check(2, 3, power(2, 3));
-  run_check(0, 0, power(0, 0));
-  run_check(1, 100, power(1, 100));
-  run_check(5, 0, power(5, 0));
-  run_check(0, 5, power(0, 5));
-  run_check(3, 4, power(3, 4));
+  run_check(2, 3, 8);
+  run_check(0, 0, 1);
+  run_check(1, 100, 1);
+  run_check(5, 0, 1);
+  run_check(0, 5, 0);
+  run_check(3, 4, 81);
+  run_check(2, 31, 2147483648);          // 2^31 = 2147483648
+  run_check(999, 2, 998001);             // 999^2 = 998001
+  run_check(1000, 3, 1000000000);        // 1000^3 = 1000000000
+  run_check(1, 1000, 1);                 // 1^1000 = 1
+  run_check(2, 16, 65536);               // 2^16 = 65536
+  run_check(4294967295, 1, 4294967295);  // UINT_MAX^1 = UINT_MAX
+  run_check(65535, 2, 4294836225);       // 65535^2 = 4294836225
 
   printf("All tests passed!\n");
   exit(EXIT_SUCCESS);
