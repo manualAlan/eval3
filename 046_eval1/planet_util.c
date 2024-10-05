f4736c159409aafee0827760f9cbaf2704d9b932
bm350 <bm350@duke.edu>
2024-10-05 11:11:20 -0400

    w

--- 046_eval1/planet_util.c
+++ 046_eval1/planet_util.c
@@ -6,8 +6,21 @@
 #include "provided.h"
 
 point_t get_location_at(const planet_t * p, double time) {
-  // Step 2: WRITE THIS
+  double angular_velocity = 2 * M_PI / p->year_len;
+  double angle = p->init_pos + angular_velocity * time;
+  angle = fmod(angle, 2 * M_PI);
+
+  point_t location;
+  location.x = p->orbital_radius * cos(angle);
+  location.y = p->orbital_radius * sin(angle);
+  return location;
 }
 planet_t * find_planet(const planet_list_t * lst, const char * name) {
-  //Step 2: WRITE THIS
+  for (size_t i = 0; i < lst->num_planets; i++) {
+    if (strncmp(lst->planets[i]->name, name, MAX_PLANET_NAME_LEN) == 0) {
+      return lst->planets[i];
+    }
+  }
+  fprintf(stderr, " Planet '%s' not found\n", name);
+  exit(EXIT_FAILURE);
 }
--- 046_eval1/target.c
+++ 046_eval1/target.c
@@ -3,10 +3,50 @@
 launch_result_t compute_launch_by_info(const launch_input_t * this_launch,
                                        const planet_list_t * planets) {
   //STEP 3: Write this
+  planet_t * src_planet = find_planet(planets, this_launch->src);
+  planet_t * dest_planet = find_planet(planets, this_launch->dest);
+
+  point_t src_location = get_location_at(src_planet, this_launch->time);
+  point_t dest_location = get_location_at(dest_planet, this_launch->time);
+
+  double dx = dest_location.x - src_location.x;
+  double dy = dest_location.y - src_location.y;
+  double distance = sqrt(dx * dx + dy * dy);
+
+  double angle = atan2(dy, dx);
+  if (angle < 0) {
+    angle += 2 * M_PI;
+  }
+
+  double duration = distance / this_launch->speed;
+
+  launch_result_t result;
+  result.theta = angle;
+  result.duration = duration;
+  return result;
 }
 
 double when_does_planet_return_to(const planet_t * planet,
                                   point_t pos,
                                   double start_time) {
-  //STEP 3: Write this
+  double target_angle = atan2(pos.y, pos.x);
+  if (target_angle < 0) {
+    target_angle += 2 * M_PI;
+  }
+
+  point_t current_position = get_location_at(planet, start_time);
+  double current_angle = atan2(current_position.y, current_position.x);
+  if (current_angle < 0) {
+    current_angle += 2 * M_PI;
+  }
+
+  double angular_velocity = 2 * M_PI / planet->year_len;
+
+  double angle_to_travel = target_angle - current_angle;
+  if (angle_to_travel < 0) {
+    angle_to_travel += 2 * M_PI;
+  }
+
+  double time_to_return = angle_to_travel / angular_velocity;
+  return start_time + time_to_return;
 }
