1eba067a3a677a81e7382dba4e914fa971b0309b
bm350 <bm350@duke.edu>
2024-10-06 19:34:06 -0400

    a

--- 046_eval1/myinput.c
+++ 046_eval1/myinput.c
@@ -1,6 +1,7 @@
 #include "myinput.h"
 
 #include <stdio.h>
+
 void parse_planet_info(planet_t * planet, char * line) {
   char * temp;
   char * line_copy = strdup(line);
@@ -13,58 +14,64 @@ void parse_planet_info(planet_t * planet, char * line) {
 
   if (field_count != 4) {
     free(line_copy);
-    printf("Wrong number of fields\n");
+    fprintf(stderr, "Wrong number of fields\n");
     exit(EXIT_FAILURE);
   }
-  //planet name
-  temp = strsep(&line, ":");
 
+  // planet name
+  temp = strsep(&line, ":");
   if (strlen(temp) >= MAX_PLANET_NAME_LEN) {
     free(line_copy);
-    printf(" Planet name longer than maximum (31) length\n");
+    fprintf(stderr, "Planet name longer than maximum (31) length\n");
     exit(EXIT_FAILURE);
   }
+
   strncpy(planet->name, temp, MAX_PLANET_NAME_LEN - 1);
   planet->name[MAX_PLANET_NAME_LEN - 1] = '\0';
-  //radius
+
+  // radius
   temp = strsep(&line, ":");
   char * endptr;
   planet->orbital_radius = strtod(temp, &endptr);
   if (*endptr != '\0') {
     free(line_copy);
-    printf(" Invalid format for orbital radius.\n");
+    fprintf(stderr, "Invalid format for orbital radius.\n");
     exit(EXIT_FAILURE);
   }
   if (planet->orbital_radius <= 0.0) {
     free(line_copy);
-    printf("orbital_radius can't be non-positive \n");
+    fprintf(stderr, "orbital_radius can't be non-positive\n");
     exit(EXIT_FAILURE);
   }
-  //period
+
+  // period
   temp = strsep(&line, ":");
   planet->year_len = strtod(temp, &endptr);
   if (*endptr != '\0') {
     free(line_copy);
-    printf(" Invalid format for period.\n");
+    fprintf(stderr, "Invalid format for period.\n");
     exit(EXIT_FAILURE);
   }
   if (planet->year_len <= 0.0) {
     free(line_copy);
-    printf("year_len can't be non-positive \n");
+    fprintf(stderr, "year_len can't be non-positive\n");
     exit(EXIT_FAILURE);
   }
-  //initial position
+
+  // initial position
   temp = strsep(&line, ":");
   if (*endptr != '\0') {
     free(line_copy);
-    printf("Invalid format for initial position.\n");
+    fprintf(stderr, "Invalid format for initial position.\n");
     exit(EXIT_FAILURE);
   }
+
   planet->init_pos = strtod(temp, NULL) * M_PI / 180.0;
   if (planet->init_pos < 0.0) {
     free(line_copy);
-    printf("init_pos can't be non-positive \n");
+    fprintf(stderr, "init_pos can't be non-positive\n");
     exit(EXIT_FAILURE);
   }
+
   free(line_copy);
 }
