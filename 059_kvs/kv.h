16de61396c8c25a9ba92112ffd8f50d23ebb0ec2
bm350 <bm350@duke.edu>
2024-10-09 16:39:11 -0400

    w

--- 059_kvs/kv.h
+++ 059_kvs/kv.h
@@ -1,20 +1,19 @@
 #ifndef __KV_H__
 #define __KV_H__
+#include <stdlib.h>
 
-
-struct _kvpair_t { 
-  //DEFINE ME!
-
+struct _kvpair_t {
+  char * key;
+  char * value;  //DEFINE ME!
 };
 typedef struct _kvpair_t kvpair_t;
 
-struct _kvarray_t { 
+struct _kvarray_t {
   //DEFINE ME!
-
-
+  kvpair_t * pairs;
+  size_t length;
 };
 typedef struct _kvarray_t kvarray_t;
-
 
 kvarray_t * readKVs(const char * fname);
 
