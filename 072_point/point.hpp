553f7ab30d0b51640c405f0e6337b91a87ad57f8
bm350 <bm350@duke.edu>
2024-10-22 11:44:44 -0400

    w

---
+++ 072_point/point.hpp
@@ -0,0 +1,15 @@
+class Point {
+ private:
+  double x;
+  double y;
+
+ public:
+  Point() : x(0), y(0) {}
+
+  void move(double dx, double dy) {
+    x += dx;
+    y += dy;
+  }
+
+  double distanceFrom(const Point & p) const;
+};
