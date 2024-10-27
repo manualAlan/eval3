86fcb7ec9027690a4926d0013e56807630b45fa0
bm350 <bm350@duke.edu>
2024-10-27 12:06:08 -0400

    w

--- 063_eval2/rand_story.c
+++ 063_eval2/rand_story.c
@@ -225,9 +225,9 @@ void parseAndPrint(const char * filename, catarray_t * cats, int noReuse) {
 
         /////////////////////////////
         // printf("%s", chosenWord);
-        if (noReuse) {
-          removeUsedWord(cats, category, chosenWord);
-        }
+        //if (noReuse) {
+        // removeUsedWord(cats, category, chosenWord);
+        //        }
         //          addWordToList(&usedWordsList.usedWords,
         //            &usedWordsList.n_used,
         //            chosenWord);  //dd to used words list
