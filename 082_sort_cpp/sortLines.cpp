1bdff05b41ad14a800010fef856b63e930b1e0d0
bm350 <bm350@duke.edu>
2024-11-01 10:22:46 -0400

    w

--- 082_sort_cpp/sortLines.cpp
+++ 082_sort_cpp/sortLines.cpp
@@ -4,7 +4,10 @@
 #include <string>
 #include <vector>
 
-void readFromStream(std::istream & stream, std::vector<std::string> & lines) {
+bool myfunction(std::string a, std::string b) {
+  return (a < b);
+}
+void read(std::istream & stream, std::vector<std::string> & lines) {
   std::string line;
   while (std::getline(stream, line)) {
     lines.push_back(line);
@@ -14,26 +17,25 @@ void readFromStream(std::istream & stream, std::vector<std::string> & lines) {
 int main(int argc, char * argv[]) {
   std::vector<std::string> lines;
   if (argc == 0) {
+    std::cerr << "how?" << std::endl;
     return EXIT_FAILURE;
   }
-  //if no arguments, read from standard input
   if (argc == 1) {
-    readFromStream(std::cin, lines);
+    read(std::cin, lines);
   }
   else {
-    //read from each file specified in the arguments
     for (int i = 1; i < argc; ++i) {
       std::ifstream file(argv[i]);
       if (!file) {
         std::cerr << "Could not open file " << argv[i] << std::endl;
         return EXIT_FAILURE;
       }
-      readFromStream(file, lines);
+      read(file, lines);
       file.close();
     }
   }
 
-  std::sort(lines.begin(), lines.end());
+  std::sort(lines.begin(), lines.end(), myfunction);
   for (std::vector<std::string>::iterator it = lines.begin(); it != lines.end(); ++it) {
     std::cout << *it << std::endl;
   }
