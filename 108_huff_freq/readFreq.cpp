65a7fb52165c71cc9812e36a3a7185bee2da93a6
bm350 <bm350@duke.edu>
2024-12-04 09:20:58 -0500

    w

--- 108_huff_freq/readFreq.cpp
+++ 108_huff_freq/readFreq.cpp
@@ -1,9 +1,14 @@
-#include <stdio.h>
-#include <cstdlib>
 #include "readFreq.h"
 
+#include <stdio.h>
+
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <string>
+
 void printSym(std::ostream & s, unsigned sym) {
-  if (sym > 256 ) {
+  if (sym > 256) {
     s << "INV";
   }
   else if (sym == 256) {
@@ -20,5 +25,18 @@ void printSym(std::ostream & s, unsigned sym) {
   }
 }
 uint64_t * readFrequencies(const char * fname) {
+  uint64_t * res = new uint64_t[257];
+  for (size_t i = 0; i < 257; i++) {
+    res[i] = 0;
+  }
+  std::ifstream ist(fname);
+  assert(ist.is_open());
+  int i;
+  while ((i = ist.get()) != EOF) {
+    ++res[i];
+  }
+  res[256] = 1;
+  return res;
+
   //WRITE ME!
 }
